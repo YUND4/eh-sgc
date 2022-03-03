9a0a9312a7decaaf60da75aaffcfbf0e41b22e1a
Swanky <santiago.yunda@puntored.co>
2022-03-03 14:37:44 -0500

    to prod

--- src/app/shared/constants/api.constants.ts
+++ src/app/shared/constants/api.constants.ts
@@ -1,3 +1,3 @@
 
-// export const BASE_URL = "/back-sgc/public"; // production
-export const BASE_URL = "http://127.0.0.1:8000"; // local
+export const BASE_URL = "/back-sgc/public"; // production
+// export const BASE_URL = "http://127.0.0.1:8000"; // local
